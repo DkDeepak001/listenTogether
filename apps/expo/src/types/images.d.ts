1cb4c5dbb028ffca43129e7dc1c85c6770a0cb5e
DkDeepak001 <Deepakeswar001@gmail.com>
2023-06-28 21:41:39 +0530

    feat: add image type

---
+++ apps/expo/src/types/images.d.ts
@@ -0,0 +1,22 @@
+declare module "*.jpg" {
+  import { type ImageSourcePropType } from "expo-image";
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module "*.png" {
+  import { type ImageSourcePropType } from "expo-image";
+  const value: ImageSourcePropType;
+  export default value;
+}
+
+declare module "*.svg" {
+  import { type ImageSourcePropType } from "expo-image";
+  const value: ImageSourcePropType;
+  export default value;
+}
+declare module "*.gif" {
+  import { type ImageSourcePropType } from "expo-image";
+  const value: ImageSourcePropType;
+  export default value;
+}
