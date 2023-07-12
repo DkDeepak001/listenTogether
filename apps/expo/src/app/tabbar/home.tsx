10b7e69c511cebd54c1cdd1c4968d02433bb9b2c
DkDeepak001 <Deepakeswar001@gmail.com>
2023-07-12 22:31:58 +0530

    feat: config expo-av  and playing sound

--- apps/expo/src/app/tabbar/home.tsx
+++ apps/expo/src/app/tabbar/home.tsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import { FlatList, Pressable, Text, View } from "react-native";
+import { Audio } from "expo-av";
 import { Image } from "expo-image";
 import { useRouter } from "expo-router";
+
+import { type Track } from "@acme/api/src/router/types";
 
 import { api } from "~/utils/api";
 import { getGreeting } from "~/utils/greeting";
 import Pill from "~/components/pill/pill";
+import play from "../../../assets/playlist/play.svg";
 import useAuthToken from "../../hooks/useAuthToken";
 
 type TopType = "tracks" | "artists";
@@ -13,6 +17,7 @@ const Home = () => {
   const router = useRouter();
   const [type, setType] = useState<TopType>("tracks");
   const { updateToken } = useAuthToken();
+  const [currnetSound, setSound] = useState<Audio.Sound | null>(null);
   const { data: user, isLoading } = api.spotify.self.useQuery();
 
   const { data: topTracks, refetch: refetchTopTracks } =
@@ -34,6 +39,34 @@ const Home = () => {
       void refetchTopTracks();
     }
   }
+
+  const handlePlay = async (item: Track) => {
+    try {
+      if (currnetSound) await currnetSound.pauseAsync();
+      await Audio.setAudioModeAsync({
+        playsInSilentModeIOS: true,
+        staysActiveInBackground: false,
+        shouldDuckAndroid: true,
+      });
+      console.log("Loading Sound", item);
+
+      const { sound } = await Audio.Sound.createAsync(
+        {
+          uri: item?.preview_url,
+        },
+        { isLooping: false, shouldPlay: true },
+      );
+
+      console.log("Loading Sound", item, sound);
+      setSound(sound);
+      await sound.playAsync();
+
+      console.log("Playing Sound");
+      await sound.playAsync();
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <View className=" flex-1  bg-black px-5 pt-5">
@@ -79,20 +112,32 @@ const Home = () => {
             <Text className="text-white">No tracks found</Text>
           )}
           renderItem={({ item }) => (
-            <View className="my-2 flex flex-row gap-x-5">
-              <Image
-                source={{ uri: item.album.images[0]?.url }}
-                className="h-16 w-16 rounded-2xl"
-                alt={item.name}
-              />
-              <View className="flex flex-col justify-center">
-                <Text className="text-lg font-extrabold text-white">
-                  {item.name}
-                </Text>
-                <Text className="text-base font-bold text-white">
-                  {item.artists[0]?.name}
-                </Text>
+            <View className="my-2 flex flex-row items-center justify-between">
+              <View className="flex w-4/5 flex-row gap-x-5">
+                <Image
+                  source={{ uri: item.album.images[0]?.url }}
+                  className="h-16 w-16 rounded-2xl"
+                  alt={item.name}
+                />
+                <View className="flex flex-col justify-center">
+                  <Text className="text-lg font-extrabold text-white">
+                    {item.name}
+                  </Text>
+                  <Text className="text-base font-bold text-white">
+                    {item.artists[0]?.name}
+                  </Text>
+                </View>
               </View>
+              <Pressable
+                className="  h-8  w-8  items-center justify-center rounded-full bg-blue-800"
+                onPress={() => void handlePlay(item)}
+              >
+                <Image
+                  className="h-4 w-4 rounded-full bg-blue-800"
+                  source={play}
+                  alt="pause"
+                />
+              </Pressable>
             </View>
           )}
         />
