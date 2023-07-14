96a7d815f912fe32b7242d5ad218ba38f0a05bcf
DkDeepak001 <Deepakeswar001@gmail.com>
2023-07-14 21:25:59 +0530

    feat: routing from search to platlist,albul,artis,song

--- apps/expo/src/app/tabbar/search.tsx
+++ apps/expo/src/app/tabbar/search.tsx
@@ -1,19 +1,25 @@
 import React, { useState } from "react";
-import { FlatList, Text, View } from "react-native";
+import { FlatList, Pressable, Text, View } from "react-native";
 import { Image } from "expo-image";
+import { useRouter } from "expo-router";
 
 import { api } from "~/utils/api";
 import Pill from "~/components/pill/pill";
 import SearchBar from "~/components/search/searchBar";
+import useAudio from "~/hooks/useAudio";
+import pause from "../../../assets/playlist/pause.svg";
+import play from "../../../assets/playlist/play.svg";
 
 export type SearchType = "tracks" | "artist" | "albums" | "playlist";
 
 const Search = () => {
+  const router = useRouter();
   const [type, setType] = useState<SearchType>("tracks");
   const [q, setQ] = useState<string>("");
   const { data: search, isLoading } = api.spotify.search.useQuery({
     q,
   });
+  const { currentTrack, handlePlay, isPlaying } = useAudio();
 
   return (
     <View className=" flex-1 bg-black p-4">
@@ -50,21 +56,38 @@ const Search = () => {
           ListEmptyComponent={() => (
             <Text className="text-white">No tracks found</Text>
           )}
-          renderItem={({ item }) => (
-            <View className="my-2 flex flex-row gap-x-5">
-              <Image
-                source={{
-                  uri: item.album.images[0]?.url ?? "",
-                }}
-                className="h-20 w-20 rounded-2xl"
-                alt={item?.name}
-                contentFit="contain"
-              />
-              <View className="flex flex-col justify-center">
-                <Text className="font-semibold text-white">{item?.name}</Text>
+          renderItem={({ item }) => {
+            if (!item?.album) return <></>;
+            return (
+              <View className="my-2 flex flex-row items-center justify-between">
+                <View className="flex flex-row items-center gap-x-2">
+                  <Image
+                    source={{
+                      uri: item.album.images[0]?.url ?? "",
+                    }}
+                    className="h-20 w-20 rounded-2xl"
+                    alt={item?.name}
+                    contentFit="contain"
+                  />
+                  <View className="flex flex-col justify-center">
+                    <Text className="font-semibold text-white">
+                      {item?.name}
+                    </Text>
+                  </View>
+                </View>
+                <Pressable
+                  className="  h-8  w-8  items-center justify-center rounded-full bg-blue-800"
+                  onPress={() => void handlePlay(item)}
+                >
+                  <Image
+                    className="h-4 w-4 rounded-full bg-blue-800"
+                    source={currentTrack === item && isPlaying ? pause : play}
+                    alt="pause"
+                  />
+                </Pressable>
               </View>
-            </View>
-          )}
+            );
+          }}
         />
       )}
 
@@ -79,19 +102,25 @@ const Search = () => {
           ListEmptyComponent={() => (
             <Text className="text-white">No artists found</Text>
           )}
-          renderItem={({ item }) => (
-            <View className="my-2 flex flex-row gap-x-5">
-              <Image
-                source={{ uri: item.images[0]?.url ?? "" }}
-                className="h-20 w-20 rounded-2xl"
-                alt={item.name}
-                contentFit="contain"
-              />
-              <View className="flex flex-col justify-center">
-                <Text className="font-semibold text-white">{item.name}</Text>
-              </View>
-            </View>
-          )}
+          renderItem={({ item }) => {
+            if (!item?.images) return <></>;
+            return (
+              <Pressable
+                className="my-2 flex flex-row gap-x-5"
+                onPress={() => router.push(`/artist/${item.id}`)}
+              >
+                <Image
+                  source={{ uri: item.images[0]?.url ?? "" }}
+                  className="h-20 w-20 rounded-2xl"
+                  alt={item.name}
+                  contentFit="contain"
+                />
+                <View className="flex flex-col justify-center">
+                  <Text className="font-semibold text-white">{item.name}</Text>
+                </View>
+              </Pressable>
+            );
+          }}
         />
       )}
 
@@ -106,21 +135,25 @@ const Search = () => {
           ListEmptyComponent={() => (
             <Text className="text-white">No albums found</Text>
           )}
-          renderItem={({ item }) => (
-            <View className="my-2 flex flex-row gap-x-5">
-              <Image
-                source={{
-                  uri: item.images[0]?.url ?? "",
-                }}
-                className="h-20 w-20 rounded-2xl"
-                alt={item.name}
-                contentFit="contain"
-              />
-              <View className="flex flex-col justify-center">
-                <Text className="font-semibold text-white">{item.name}</Text>
-              </View>
-            </View>
-          )}
+          renderItem={({ item }) => {
+            if (!item?.images) return <></>;
+            return (
+              <Pressable
+                className="my-2 flex flex-row gap-x-5"
+                onPress={() => router.push(`/album/${item.id}`)}
+              >
+                <Image
+                  source={{ uri: item.images[0]?.url ?? "" }}
+                  className="h-20 w-20 rounded-2xl"
+                  alt={item.name}
+                  contentFit="contain"
+                />
+                <View className="flex flex-col justify-center">
+                  <Text className="font-semibold text-white">{item.name}</Text>
+                </View>
+              </Pressable>
+            );
+          }}
         />
       )}
 
@@ -135,21 +168,25 @@ const Search = () => {
           ListEmptyComponent={() => (
             <Text className="text-white">No playlists found</Text>
           )}
-          renderItem={({ item }) => (
-            <View className="my-2 flex flex-row gap-x-5">
-              <Image
-                source={{
-                  uri: item.images[0]?.url ?? "",
-                }}
-                className="h-20 w-20 rounded-2xl"
-                alt={item.name}
-                contentFit="contain"
-              />
-              <View className="flex flex-col justify-center">
-                <Text className="font-semibold text-white">{item.name}</Text>
-              </View>
-            </View>
-          )}
+          renderItem={({ item }) => {
+            if (!item?.images) return <></>;
+            return (
+              <Pressable
+                className="my-2 flex flex-row gap-x-5"
+                onPress={() => router.push(`/playlist/${item.id}`)}
+              >
+                <Image
+                  source={{ uri: item.images[0]?.url ?? "" }}
+                  className="h-20 w-20 rounded-2xl"
+                  alt={item.name}
+                  contentFit="contain"
+                />
+                <View className="flex flex-col justify-center">
+                  <Text className="font-semibold text-white">{item.name}</Text>
+                </View>
+              </Pressable>
+            );
+          }}
         />
       )}
     </View>
